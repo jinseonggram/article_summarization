faee20c750b44b655a379c5edacdcd68da1d2c99
jinseonggram <king7762225@gmail.com>
2022-11-02 07:48:00 +0900

    chore : Add progress bar

--- trainer.py
+++ trainer.py
@@ -1,6 +1,7 @@
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import TQDMProgressBar
 
 
 class MyTrainer:
@@ -19,7 +20,7 @@ class MyTrainer:
     def train(self, model, data_module):
         trainer = pl.Trainer(
             logger=self.logger,
-            callbacks=[self.checkpoint_callback],
+            callbacks=[self.checkpoint_callback, TQDMProgressBar(refresh_rate=10)],
             gpus=1,
             max_epochs=self.config.n_epochs,
         )
