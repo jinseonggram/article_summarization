c723eb78f22d02f4a44329469a575d15cc43bf24
jinseonggram <king7762225@gmail.com>
2022-11-03 09:52:43 +0900

    feat : Add predict file

--- predict.py
+++ predict.py
@@ -1,21 +1,14 @@
 import pytorch_lightning as pl
 from transformers import (
-    AdamW,
-    T5ForConditionalGeneration,
-    T5TokenizerFast as T5Tokenizer
+    AdamW, BartTokenizer, BartForConditionalGeneration
 )
-from transformers import BartTokenizer, BartForConditionalGeneration
-MODEL_NAME = "facebook/bart-large-cnn"
-toekenizer = BartTokenizer.from_pretrained(MODEL_NAME)
-
+import torch
 
 
 class NewsSummaryModel(pl.LightningDataModule):
-
     def __init__(self):
         super().__init__()
-        # self.model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME, return_dict=True)
-        self.model = BartForConditionalGeneration.from_pretrained(MODEL_NAME, return_dict=True)
+        self.model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn", return_dict=True)
 
     def forward(self, input_ids, attention_mask, decoder_attention_mask, labels=None):
 
@@ -79,54 +72,44 @@ class NewsSummaryModel(pl.LightningDataModule):
     def configure_optimizers(self):
         return AdamW(self.parameters(), lr=0.0001)
 
-trained_model = NewsSummaryModel.load_from_checkpoint(
-    './model/best.ckpt'
-)
 
-trained_model.freeze()
+class Inference:
+    def __init__(self, model_path='./model/best.ckpt'):
+        # self.trained_model = NewsSummaryModel.load_from_checkpoint(model_path)
+        self.model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+        self.tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
 
-def summarize(text):
-    text_encoding = toekenizer(
-        text,
-        max_length=512,
-        padding="max_length",
-        truncation=True,
-        return_attention_mask=True,
-        add_special_tokens=True,
-        return_tensors='pt'
-               )
+    def summarize(self, text):
+        text_len = len(self.tokenizer.encode(text))
 
-    generated_ids = trained_model.model.generate(
-        input_ids=text_encoding['input_ids'],
-        attention_mask=text_encoding['attention_mask'],
-        max_length=150,
-        num_beams=2,
-        repetition_penalty=2.5,
-        length_penalty=1.0,
-        early_stopping=True
-    )
+        if text_len >= 1200:
+            inputs_no_trunc = self.tokenizer(text, max_length=None, return_tensors='pt', truncation=False)
+            chunk_start = 0
+            chunk_end = self.tokenizer.model_max_length  # == 1024 for Bart
+            inputs_batch_lst = []
+            while chunk_start <= len(inputs_no_trunc['input_ids'][0]):
+                inputs_batch = inputs_no_trunc['input_ids'][0][chunk_start:chunk_end]  # get batch of n tokens
+                inputs_batch = torch.unsqueeze(inputs_batch, 0)
+                inputs_batch_lst.append(inputs_batch)
+                chunk_start += self.tokenizer.model_max_length  # == 1024 for Bart
+                chunk_end += self.tokenizer.model_max_length  # == 1024 for Bart
 
-    preds = [
-        toekenizer.decode(gen_id, skip_special_tokens=True, clean_up_tokenization_spaces=True)
-        for gen_id in generated_ids
-    ]
+            # generate a summary on each batch
+            # summary_ids_lst = [self.trained_model.model.generate(inputs, num_beams=4, max_length=256, early_stopping=True) for inputs in inputs_batch_lst]
+            summary_ids_lst = [self.model.generate(inputs, num_beams=4, max_length=256, early_stopping=True) for inputs in inputs_batch_lst]
 
-    return "".join(preds)
+            # decode the output and join into one string with one paragraph per summary batch
+            summary_batch_lst = []
+            for summary_id in summary_ids_lst:
+                summary_batch = [self.tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_id]
+                summary_batch_lst.append(summary_batch[0])
+            summary_all = '\n'.join(summary_batch_lst)
+        if text_len < 1500:
+            inputs = self.tokenizer(text, mask_token=1024, truncation=True, return_tensors='pt')
+            # generated_ids = self.trained_model.model.generate(inputs['input_ids'], num_beams=4, max_length=256, early_stopping=True)
+            generated_ids = self.model.generate(inputs['input_ids'], num_beams=4, max_length=256, early_stopping=True)
+            preds = [self.tokenizer.decode(gen_id, skip_special_tokens=True, clean_up_tokenization_spaces=False) for gen_id in generated_ids]
+            summary_all = "".join(preds)
 
-# sample_row = test_df.iloc[0]
-# text = sample_row['text']
-text = "North Korea fired at least 10 missiles of various types from its east and west coasts on Wednesday, " \
-       "South Korea’s Ministry of National Defense said. South Korea’s Joint Chiefs of Staff (JCS) said the launches " \
-       "mark the first time a North Korean ballistic missile has fallen close to South Korea’s territorial waters – " \
-       "south of the Northern Limit Line – since the division of Korea. The barrage of missile tests set off an air " \
-       "raid warning in South Korea’s Ulleungdo island that sits about 120 kilometers (75 miiles) east of the Korean " \
-       "Peninsula. JCS said one short-range ballistic missile fell in the international waters 167 kilometers (104 " \
-       "miles) northwest of the island. Wednesday’s launch is North Korea’s 29th this year, according to a CNN count, " \
-       "and comes after the United States and South Korea began previously scheduled military exercises called " \
-       "“Vigilant Storm” on Tuesday. The maneuvers involve 240 aircraft and “thousands of service members” from both " \
-       "countries, according to the US Defense Department. US Defense Secretary Lloyd Austin is scheduled to meet " \
-       "with his South Korean counterpart Lee Jong-sup at the Pentagon on Thursday. "
-model_summary = summarize(text)
-print(model_summary)
-
+        return summary_all
 
